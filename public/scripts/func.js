6ab1d4fbe460b47668dfc86b922c8c3a1cd5a83d
Jesse Lind <Jesseclind@gmail.com>
2021-01-25 18:37:51 -0500

    It's live!!!!!

---
+++ public/scripts/func.js
@@ -0,0 +1,71 @@
+// Holds userful functions for other js scripts 
+
+// Find total time so far for each element, returns object with array of hours, minutes, seconds, and milliseconds
+const findElapsedTime = (index, arr) => {
+    let totH = 0, totM = 0, totS = 0, totMs = 0;
+    for (let i = 0; i < index + 1; i++) {
+        totH += arr[i].time.hours;
+        totM += arr[i].time.minutes;
+        totS += arr[i].time.seconds;
+        totMs += arr[i].time.milliseconds
+
+        // fix additions of times. i.e. add to minutes if seconds is over or equal to 60 seconds. 
+        if (totMs >= 100) {
+            totMs -= 100;
+            totS++;
+        }
+        if (totS >= 60) {
+            totS -= 60;
+            totM++; 
+        }
+        if (totM >= 60) {
+            totM -= 60; 
+            totH++;
+        }
+    }
+    return {hours: totH, minutes: totM, seconds: totS, milliseconds: totMs};
+}
+
+
+
+
+
+// Convert time string to number of milliseconds
+const stringToMS = (obj) => {
+    let hours = obj.hours
+    let minutes = obj.minutes
+    let seconds = obj.seconds
+    let milliseconds = obj.milliseconds
+    
+    let totalMS = (hours * 3600000) + (minutes * 60000) + (seconds * 1000) + (milliseconds * 10)
+    
+    return totalMS;
+}
+// Convert number of milliseconds to formated time string
+function msToString(ms) {
+    let hours = 0, minutes = 0, seconds = 0
+    if (ms >= 3600000) {
+        console.log(ms)
+        hours = Math.floor(ms / 3600000)
+        ms = ms % 3600000
+        console.log(hours, ms)
+    }
+    if (ms >= 60000) {
+        minutes = Math.floor(ms / 60000)
+        ms = ms % 60000
+    }
+    if (ms >= 1000) {
+        seconds = Math.floor(ms / 1000)
+        ms = ms % 1000
+    }
+
+    return hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds) + "." + (ms < 10 ? "0" + ms : ms)
+}
+
+// Compares if time strings are greater/equal/less than each other.
+// If -1 a < b, if 0 a = b, if 1 a > b
+const compareTimeObjects = (a, b) => {
+    let aStr = `${a.hours}:${(a.minutes < 10 ? "0" + a.minutes : a.minutes)}:${(a.seconds < 10 ? "0" + a.seconds : a.seconds)}.${(a.milliseconds < 10 ? "0" + a.milliseconds : a.milliseconds)}`
+    let bStr = `${b.hours}:${(b.minutes < 10 ? "0" + b.minutes : b.minutes)}:${(b.seconds < 10 ? "0" + b.seconds : b.seconds)}.${(b.milliseconds < 10 ? "0" + b.milliseconds : b.milliseconds)}`
+    return aStr.localeCompare(bStr);
+}
