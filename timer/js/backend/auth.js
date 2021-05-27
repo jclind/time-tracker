c7b197a828e5583e6d25bb9382bde97fffb72191
Jesse Lind <Jesseclind@gmail.com>
2021-05-27 14:41:27 -0400

    Fix small bug where tags would not update correctly on loggin

--- timer/js/backend/auth.js
+++ timer/js/backend/auth.js
@@ -2,6 +2,29 @@ const loggedOutLinks = document.querySelectorAll('.logged-out')
 const loggedInLinks = document.querySelectorAll('.logged-in')
 
 const setupUI = user => {
+    const setActiveTag = () => {
+        const lastTimesInfoEl = timesInfoList[timesInfoList.length - 1]
+        // Check if timeTags is empty before using accessing it's data
+        if (timeTags.length === 0) {
+            timeTags.push({ name: 'Main', color: '#fd483f' })
+        }
+        // If timesInfoList isn't empty, change tag button name to last submitted tag name
+        if (timesInfoList.length > 0) {
+            document.getElementById('activeTimeTagSelectionBtnName').innerText =
+                lastTimesInfoEl.tag.name
+            document.getElementById(
+                'activeTimeTagSelectionBtnColor'
+            ).style.color = lastTimesInfoEl.tag.color
+        } else {
+            // Set active tag to the first tag in the timeTags array
+            document.getElementById('activeTimeTagSelectionBtnName').innerText =
+                timeTags[0].name
+            document.getElementById(
+                'activeTimeTagSelectionBtnColor'
+            ).style.color = timeTags[0].color
+        }
+    }
+
     if (user) {
         console.log('state change')
         // User Logs in
@@ -35,6 +58,7 @@ const setupUI = user => {
                     )
                     accountModalEmailInput.value = user.email
                     accountModalNameInput.value = doc.data().name
+                    setActiveTag()
                     updateTimeTable(timesInfoList)
                 })
         } else {
@@ -66,6 +90,7 @@ const setupUI = user => {
                         )
                         accountModalEmailInput.value = user.email
                         accountModalNameInput.value = doc.data().name
+                        setActiveTag()
                         updateTimeTable(timesInfoList)
                         $('#saveTimesPromptModal').modal('hide')
                     })
@@ -91,7 +116,7 @@ const setupUI = user => {
                         accountModalEmailInput.value = user.email
                         accountModalNameInput.value = doc.data().name
                         console.log('this is a test')
-
+                        setActiveTag()
                         $('#saveTimesPromptModal').modal('hide')
                     })
             })
@@ -114,26 +139,8 @@ const setupUI = user => {
         })
         accountModalNameInput.value = 'No User Signed In'
         accountModalEmailInput.value = 'No User Signed In'
+        setActiveTag()
         updateTimeTable(timesInfoList)
-    }
-
-    let lastTimesInfoEl = timesInfoList[timesInfoList.length - 1]
-    // Check if timeTags is empty before using accessing it's data
-    if (timeTags.length === 0) {
-        timeTags.push({ name: 'Main', color: '#fd483f' })
-    }
-    // If timesInfoList isn't empty, change tag button name to last submitted tag name
-    if (timesInfoList.length > 0) {
-        document.getElementById('activeTimeTagSelectionBtnName').innerText =
-            lastTimesInfoEl.tag.name
-        document.getElementById('activeTimeTagSelectionBtnColor').style.color =
-            lastTimesInfoEl.tag.color
-    } else {
-        // Set active tag to the first tag in the timeTags array
-        document.getElementById('activeTimeTagSelectionBtnName').innerText =
-            timeTags[0].name
-        document.getElementById('activeTimeTagSelectionBtnColor').style.color =
-            timeTags[0].color
     }
 }
 
